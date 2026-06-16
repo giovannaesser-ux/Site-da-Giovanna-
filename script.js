1bd8c5c5610845219a7e5642925ea9941f077297
giovannaesser-ux <giovanna.esser@escola.pr.gov.br>
2026-06-16 19:33:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.getElementById(“botao”);
 
 botao.addEventListener(“click”, function() {
-alert(“💖 Obrigada por visitar o Site da Giovanna! 🌸✨”);
+alert(“⭐️ Obrigada por visitar o Site da Giovanna! ⭐️”);
 });
